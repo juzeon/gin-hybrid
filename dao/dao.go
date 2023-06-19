4e8060e237cb1b9c184258430f1820a1be1596b3
juzeon <812312770@qq.com>
2023-06-19 10:55:31 +0800

    feat(dao): add dao package with setup and transaction methods

---
+++ dao/dao.go
@@ -0,0 +1,168 @@
+package dao
+
+import (
+	"errors"
+	"fmt"
+	"gin-hybrid/conf"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
+	"time"
+)
+
+func MustSetup[T any](srvConf *conf.ServiceConfig[T]) (*gorm.DB, error) {
+	db, err := Setup(srvConf)
+	if err != nil {
+		panic(err)
+	}
+	return db, nil
+}
+func Setup[T any](srvConf *conf.ServiceConfig[T]) (*gorm.DB, error) {
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logger.Info, // Log level
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+			Colorful:                  true,
+		},
+	)
+	opts := &gorm.Config{}
+	opts.Logger = newLogger
+	db, err := gorm.Open(mysql.Open(fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local",
+		srvConf.ParentConf.DB.User,
+		srvConf.ParentConf.DB.Pass,
+		srvConf.ParentConf.DB.Host,
+		srvConf.ParentConf.DB.Port,
+		srvConf.ParentConf.DB.DB)), opts)
+	if err != nil {
+		return nil, errors.New("cannot connect to database")
+	}
+	return db, nil
+}
+
+type TxWrapper[T any] struct {
+	Tx *gorm.DB
+}
+
+func NewTxWrapper[T any](tx *gorm.DB) *TxWrapper[T] {
+	return &TxWrapper[T]{tx}
+}
+
+func (t *TxWrapper[T]) FindOne(conditions ...any) (*T, error) {
+	var obj T
+	err := t.Tx.Take(&obj, conditions...).Error
+	if err == gorm.ErrRecordNotFound {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+	return &obj, nil
+}
+func (t *TxWrapper[T]) MustFindOne(conditions ...any) *T {
+	obj, err := t.FindOne(conditions...)
+	if err != nil {
+		panic(err)
+	}
+	return obj
+}
+func (t *TxWrapper[T]) FindMany(conditions ...any) ([]T, error) {
+	var arr []T
+	err := t.Tx.Find(&arr, conditions...).Error
+	if err != nil {
+		return nil, err
+	}
+	return arr, nil
+}
+func (t *TxWrapper[T]) MustFindMany(conditions ...any) []T {
+	arr, err := t.FindMany(conditions...)
+	if err != nil {
+		panic(err)
+	}
+	return arr
+}
+func (t *TxWrapper[T]) MustCreate(obj *T) {
+	err := t.Tx.Create(&obj).Error
+	if err != nil {
+		panic(err)
+	}
+}
+func (t *TxWrapper[T]) MustCreateMany(arr []T) {
+	err := t.Tx.Create(&arr).Error
+	if err != nil {
+		panic(err)
+	}
+}
+func (t *TxWrapper[T]) MustSave(obj *T) {
+	err := t.Tx.Save(&obj).Error
+	if err != nil {
+		panic(err)
+	}
+}
+func (t *TxWrapper[T]) MustDelete() {
+	var empty T
+	err := t.Tx.Delete(&empty).Error
+	if err != nil {
+		panic(err)
+	}
+}
+func (t *TxWrapper[T]) MustUpdates(values any) {
+	var err error
+	if t.Tx.Statement.Model == nil {
+		var empty T
+		err = t.Tx.Model(&empty).Updates(values).Error
+	} else {
+		err = t.Tx.Updates(values).Error
+	}
+	if err != nil {
+		panic(err)
+	}
+}
+func (t *TxWrapper[T]) MustUpdate(column string, value any) {
+	var err error
+	if t.Tx.Statement.Model == nil {
+		var empty T
+		err = t.Tx.Model(&empty).Update(column, value).Error
+	} else {
+		err = t.Tx.Update(column, value).Error
+	}
+	if err != nil {
+		panic(err)
+	}
+}
+func (t *TxWrapper[T]) Where(query any, args ...any) *TxWrapper[T] {
+	return NewTxWrapper[T](t.Tx.Where(query, args...))
+}
+func (t *TxWrapper[T]) Select(query any, args ...any) *TxWrapper[T] {
+	return NewTxWrapper[T](t.Tx.Select(query, args...))
+}
+func (t *TxWrapper[T]) Raw(sql string, values ...any) *TxWrapper[T] {
+	return NewTxWrapper[T](t.Tx.Raw(sql, values...))
+}
+func (t *TxWrapper[T]) MustExec(sql string, values ...any) {
+	err := t.Tx.Exec(sql, values...).Error
+	if err != nil {
+		panic(err)
+	}
+}
+func (t *TxWrapper[T]) Model(value any) *TxWrapper[T] {
+	return NewTxWrapper[T](t.Tx.Model(value))
+}
+func (t *TxWrapper[T]) Order(value any) *TxWrapper[T] {
+	return NewTxWrapper[T](t.Tx.Order(value))
+}
+func (t *TxWrapper[T]) MustScan(dest any) {
+	var err error
+	if t.Tx.Statement.Model == nil {
+		var empty T
+		err = t.Tx.Model(&empty).Scan(&dest).Error
+	} else {
+		err = t.Tx.Scan(&dest).Error
+	}
+	if err != nil {
+		panic(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -13,6 +13,8 @@ require (
 	github.com/xeonx/timeago v1.0.0-rc5
 	go.etcd.io/etcd/api/v3 v3.5.9
 	go.etcd.io/etcd/client/v3 v3.5.9
+	gorm.io/driver/mysql v1.5.1
+	gorm.io/gorm v1.25.1
 )
 
 require (
@@ -25,12 +27,15 @@ require (
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.1 // indirect
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/goccy/go-json v0.9.11 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/uuid v1.1.2 // indirect
 	github.com/huandu/xstrings v1.3.3 // indirect
 	github.com/imdario/mergo v0.3.11 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
